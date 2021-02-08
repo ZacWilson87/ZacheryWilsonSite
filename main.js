4d546ca255868a41bb578da5e2563294e671d1b8
unknown <zilson1987@gmail.com>
2021-02-07 22:31:09 -0600

    adding a sticky navigation bar, work in progress

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-//Make the "hery" of Zachery dissapear
+let getNamePtag = document.getElementById("yourName");
+let innerText = getNamePtag.innerText;
+let nameToShorten = "Zachery Wilson"
+
+// splits name at space converts to array
+nameToShorten = nameToShorten.split(" ");
 /*
-    let getNamePtag = document.getElementById("yourName");
-    let innerText = getNamePtag.innerText;
-    let nameShorten = "Zac Wilson";
+function slices 1 letter off first name, updates DOM, 
+checks length: repeats until first name = "Zac"
+*/
 
-    setTimeout( () => {
-        
-            getNamePtag.innerText = nameShorten;
-    }, 2000);
-  */
- 
- let getNamePtag = document.getElementById("yourName");
- let innerText = getNamePtag.innerText;
- let nameToShorten = "Zachery Wilson"
-
- // splits name at space converts to array
- nameToShorten =  nameToShorten.split(" "); 
-
- //reverse for loop that stops when name reaches "Zac"
- //for(let i = nameToShorten[0].length-1; i >= 3; i-- ){
- //        setTimeout( () => {
- //               //starts at 0 index of name string & removes last letter 1 at end of first name
- //               nameToShorten[0] = nameToShorten[0].slice(0, i); 
- //               //console.log(nameToShorten);
- //               getNamePtag.innerText = nameToShorten[0] + " " + nameToShorten[1];
- //        }, 1000);
- //        
- //}
-
- function nameSlicer(){
-         setTimeout( () => {
-                nameToShorten[0] = nameToShorten[0].slice(0, nameToShorten[0].length-1);
+function nameSlicer() {
+        setTimeout(() => {
+                nameToShorten[0] = nameToShorten[0].slice(0, nameToShorten[0].length - 1);
                 getNamePtag.innerText = nameToShorten[0] + " " + nameToShorten[1];
                 console.log(nameToShorten[0]);
-                if(nameToShorten[0].length > 3){
+                if (nameToShorten[0].length > 3) {
                         nameSlicer(nameToShorten[0]);
+
                 }
-         },600);
- }
- nameSlicer(nameToShorten[0]);
- 
+        }, 600);
+}
+
+nameSlicer();
+
+//when user scrolls menu-bar will stay at top of screen "Sticky Navigation Bar"
+
+window.onscroll = function () { stickyMenuBar() };
+
+var menuBar = document.getElementById("menu-bar");
+var sticky = menuBar.offsetTop;
+function stickyMenuBar() {
+        if (window.pageYOffset >= sticky) {
+                menuBar.classList.add("sticky");
+        } else {
+                menuBar.classList.remove("sticky");
+        }
+}
+
+ //setInterval(nameSlicer(nameToShorten[0]), 6000) ;
+
 
