6899d399670947ca4a748d3c425c46c3a560b5fb
Zac <zilson19878@gmail.com>
2021-02-10 16:26:43 -0600

    functionality to repeat name slicing over and over

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ let getNamePtag = document.getElementById("yourName");
 let innerText = getNamePtag.innerText;
 let nameToShorten = "Zachery Wilson"
 
-// splits name at space converts to array
-nameToShorten = nameToShorten.split(" ");
 /*
 function slices 1 letter off first name, updates DOM, 
 checks length: repeats until first name = "Zac"
 */
-
-function nameSlicer() {
+function nameSlicer(name) {
         setTimeout(() => {
-                nameToShorten[0] = nameToShorten[0].slice(0, nameToShorten[0].length - 1);
-                getNamePtag.innerText = nameToShorten[0] + " " + nameToShorten[1];
-                console.log(nameToShorten[0]);
-                if (nameToShorten[0].length > 3) {
-                        nameSlicer(nameToShorten[0]);
-
+                let nameArr = name.split(" ");
+                getNamePtag.innerText = nameArr[0] + " " + nameArr[1];
+                nameArr[0] = nameArr[0].slice(0, nameArr[0].length - 1);
+                console.log(nameArr[0]);
+                if (nameArr[0].length > 2) {
+                        nameSlicer(nameArr[0] + " " + nameArr[1]);
+                } else {
+                        setTimeout(() => {
+                                nameSlicer(nameToShorten);
+                        }, 1000);
                 }
         }, 700);
 }
 
-nameSlicer();
+nameSlicer(nameToShorten);
 
 //when user scrolls menu-bar will stay at top of screen "Sticky Navigation Bar"
 
